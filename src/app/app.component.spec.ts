a62d3b1cb819129f58d014c8c6d5285568e7657e
Hugh <hcmpercival@gmail.com>
2018-01-30 22:34:45 +0000

    tests pass and add chrome to travis

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,6 +2,7 @@ import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
 import { LoginComponent } from './login/login.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
@@ -11,6 +12,7 @@ describe('AppComponent', () => {
         MainComponent,
         LoginComponent
       ],
+      imports: [ReactiveFormsModule],
     }).compileComponents();
   }));
   it('should create the app', async(() => {
--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { LoginComponent } from './login.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { SettingsService } from '../services/settings.service';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,9 +10,11 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [LoginComponent],
+      imports: [ReactiveFormsModule],
+      providers: [SettingsService],
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
