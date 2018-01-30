df7d6c33d6ade436baa7541aff76b57a9444c6da
Hugh <hcmpercival@gmail.com>
2018-01-30 22:49:26 +0000

    travis test

--- karma.conf.js
+++ karma.conf.js
@@ -27,7 +27,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'ChromeHeadless', 'ChromeHeadlessNoSandbox'],
+    browsers: ['ChromeHeadlessNoSandbox'],
     singleRun: false,
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
