293b9abb54bf544cf2231bf36f46edaaa3457a4b
Hugh <hcmpercival@gmail.com>
2018-03-25 15:40:31 +0100

    some front end + fix server image path

--- back-end/src/api-server.ts
+++ back-end/src/api-server.ts
@@ -4,6 +4,7 @@ import * as bodyParser from 'body-parser';
 import './logger/logger';
 import * as cookieParser from 'cookie-parser';
 import { authMiddleware } from './api/auth/router-middleware';
+import * as path from 'path';
 
 export const app = express();
 
@@ -14,5 +15,6 @@ app.use('/api', apiRouter);
 
 /* istanbul ignore next */
 app.get('/img/server-icons/:img', authMiddleware, (req, res) => {
-  res.sendFile(`${__dirname}/public/img/server-icons/${req.params.img}`);
+  const url = path.join(__dirname, '../dist/public/img/server-icons', req.params.img);
+  res.sendFile(url);
 });
