d07950de4c77a1e647e416c3fec695f5c06984e4
Hugh <hcmpercival@gmail.com>
2019-01-02 00:31:48 +0000

    refactor chatmessage pipe and add tests for links

--- src/app/pipes/chatmessage.pipe.spec.ts
+++ src/app/pipes/chatmessage.pipe.spec.ts
@@ -3,18 +3,14 @@ import { ChatMessagePipe } from './chatmessage.pipe';
 describe('Chatmessage Pipe', () => {
   let pipe: ChatMessagePipe;
   const fakeSanitizer: any = {
-    sanitize: jasmine.createSpy().and.callFake((_, val) => `sanitized ${val}`),
-    bypassSecurityTrustUrl: jasmine.createSpy().and.callFake(val => `bypassed ${val}`),
+    sanitize: (_, val) => `sanitized ${val}`,
+    bypassSecurityTrustUrl: val => `bypassed ${val}`,
   };
 
   beforeEach(() => {
     pipe = new ChatMessagePipe(fakeSanitizer);
   });
 
-  afterEach(() => {
-    fakeSanitizer.sanitize.calls.reset();
-    fakeSanitizer.bypassSecurityTrustUrl.calls.reset();
-  });
   it('transforms a message', () => {
     expect(pipe.transform('hello')).toEqual('sanitized bypassed hello');
   });
@@ -26,6 +22,21 @@ describe('Chatmessage Pipe', () => {
   it('creates <pre> tag around code twice', () => {
     expect(pipe.transform('hello `code` here `again`')).toEqual(
       'sanitized bypassed hello <pre class="chat-pre">code</pre> here <pre class="chat-pre">again</pre>',
+    );
+  });
+  it('converts links to <a> tags', () => {
+    expect(pipe.transform('google.com')).toEqual(
+      `sanitized bypassed <a href="https://google.com" class="chat-link" target="_blank">google.com</a>`,
+    );
+  });
+  it('converts links starting with https://www. to <a> tags', () => {
+    expect(pipe.transform('https://www.google.com')).toEqual(
+      `sanitized bypassed <a href="https://www.google.com" class="chat-link" target="_blank">https://www.google.com</a>`,
+    );
+  });
+  it('converts links starting with www. to <a> tags', () => {
+    expect(pipe.transform('www.google.com')).toEqual(
+      `sanitized bypassed <a href="https://www.google.com" class="chat-link" target="_blank">www.google.com</a>`,
     );
   });
 });
--- src/app/pipes/chatmessage.pipe.ts
+++ src/app/pipes/chatmessage.pipe.ts
@@ -13,21 +13,35 @@ export class ChatMessagePipe implements PipeTransform {
 
   // Modify this method according to your custom logic
   private stylize(str: string): string {
-    let result = str ? linkifyString(str, { className: 'chat-link' }) : str;
-
-    let matches = /\`(.*?)\`/.exec(result);
-    let i = 0;
-    while (matches) {
-      result = result.replace(matches[0], `<pre class="chat-pre">${matches[1]}</pre>`);
-      matches = /\`(.*?)\`/.exec(result);
-      if (i++ >= 100) {
-        break;
-      }
-    }
+    const result = str
+      ? this.combineFunctions(str, this.links, this.codePreformatted)
+      : str;
 
     return this._domSanitizer.sanitize(
       SecurityContext.URL,
       this._domSanitizer.bypassSecurityTrustUrl(result),
     );
+  }
+
+  private combineFunctions(str: string, ...funcs: Array<(string) => string>) {
+    return funcs.reduce((result, func) => func(result), str);
+  }
+
+  /** Converts links to <a> tags */
+  private links(str: string): string {
+    return linkifyString(str, { className: 'chat-link', defaultProtocol: 'https' });
+  }
+
+  /** Converts text within quotes (``) to <pre> tags */
+  private codePreformatted(str: string): string {
+    const regex = /\`(.*?)\`/;
+    let matches = regex.exec(str);
+
+    while (matches) {
+      str = str.replace(matches[0], `<pre class="chat-pre">${matches[1]}</pre>`);
+      matches = regex.exec(str);
+    }
+
+    return str;
   }
 }
